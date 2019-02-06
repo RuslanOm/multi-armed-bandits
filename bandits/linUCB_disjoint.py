62d6f420f3e9594ad3a690f01f42e3ed479cde5d
RuslanOm <38555638+RuslanOm@users.noreply.github.com>
2019-02-06 12:28:33 +0300

    Update linUCB_disjoint.py

--- bandits/linUCB_disjoint.py
+++ bandits/linUCB_disjoint.py
@@ -2,78 +2,120 @@
 
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class ContextBandit:
 
-    def __init__(self, d, groups, users, alpha):
+    def __init__(self, alpha):
         """Initialization of our bandit class
-
-        d: int, dimension of context vectors of users and groups
-
-        groups: dict(groupId: group_context), the dictionary of group context_vectors
-
-        users: list(real_reward, groupId, user_context), the list of tuples, which imitates real process
-
         alpha: float, parameter of algorithm
         """
-        self.users = users
-        self.groups = groups
         self.alpha = alpha
-        self._d = d
 
-        # матрицы, прямая и обратная, соответствующие группам
+    def read_txt(self, directory):
+        """Format of txt file: id of group/article/arm, reward, user context
+        """
+        with open(directory, "r") as f:
+            data = f.readlines()
 
-        self._Aa = {key: np.dot(groups[key].transpose, groups[key]) for key in groups}
-        self._Aa_inv = {key: np.linalg.inv(self._Aa[key]) for key in self._Aa}
+        arms = set()
+        data = [item.split() for item in data]
 
-        # вот здесь вот тонкий момент с инициализацией, поскольку у них изначально весь контекст по статьям прям голый
-        # и нулевой вектор естественным образом нормально вписывается, но у нас уже есть какая-то стартовая информация
-        # по поводу групп и это уже отражено в матрице Aa, но никак не отражается в векторе b_a; так и оставить пока?
+        user_log = []
+        for item in data:
+            arms.add(item[0])
+            user_log.append([item[0], int(item[1]), np.array([list(map(int, item[2:]))])])
 
-        self._b_a = {key: np.zeros(self._d, 1) for key in groups}
-        self.theta_a = {key: np.zeros(self._d, 1) for key in groups}
+        np.random.shuffle(user_log)
+        self.arms = arms
+        self.user_log = user_log
+        self.n_context = len(item[2:])
 
-    def recommend(self):
-        for reward, group, context in self.users:
-            # инициализируем словарь ожидаемых "выплат" для каждой руки
-            payoffs = {key: -1 for key in self._Aa}
+        self.CTR = {key: {} for key in self.arms}
+        self.CTR_relative = {key: {} for key in self.arms}
 
-            # будем считать максимальную выплату сразу же по мере их подсчета
-            my_max = -100000000
-            max_key = -1
+        self.ctr_num = {key: 0 for key in self.arms}
+        self.ctr_den = {key: 0 for key in self.arms}
 
-            # просто цикл из алгоритма для каждой руки
-            for key in self._Aa_inv:
-                self.theta_a[key] = np.dot(self._Aa_inv[key], self._b_a[key])
-                payoffs[key] = self.theta_a[key] * context + \
-                               self.alpha * np.sqrt(np.dot(np.dot(context.transpose(), self._Aa_inv[key]), context))
-                max_key = max_key if payoffs[key] < my_max else key
-                my_max = my_max if payoffs[key] < my_max else payoffs[key]
+    def read_csv(self, directory):
+        """Format of csv file: id of group/article/arm, reward, user context(ndarray (1, n_context))
+        """
+        data = pd.read_csv(directory)
 
-            # если угадали с выбором, то обновляем соотвествующие матрицы и вектора
-            if max_key == group:
-                self.update(max_key, reward, context)
+        self.user_log = list(zip(data["group"], data["reward"], data["context"]))
+        np.random.shuffle(self.user_log)
 
-    def update(self, key, reward, context):
-        self._Aa[key] += np.dot(context.transpose(), context)
-        self._Aa_inv[key] = np.linalg.inv(self._Aa[key])
-        self._b_a += reward * context
+        self.arms = set(data["group"])
+        self.n_context = len(data["context"][0])
 
-    def calc_ctr(self):
-        # пока ещё не придумал как подсчитывать
-        pass
+        self.CTR = {key: {} for key in self.arms}
+        self.CTR_relative = {key: {} for key in self.arms}
 
-    # добавление группы в пул групп
-    def add_arm(self, group, context=None):
-        if context is None:
-            self.groups[group] = None
-            self._Aa[group] = np.identity(self._d)
-            self._Aa_inv[group] = np.identity(self._d)
-            self._b_a[group] = np.zeros((self._d, 1))
-            self.theta_a[group] = np.zeros((self._d, 1))
+        self.ctr_num = {key: 0 for key in self.arms}
+        self.ctr_den = {key: 0 for key in self.arms}
 
-    # удаление группы из пула групп
-    def delete_arm(self, group):
-        del self.groups[group]
+    def fit(self):
+        time = 0
+        self.Aa = {key: np.identity(self.n_context) for key in self.arms}
+        self.Aa_inv = {key: np.identity(self.n_context) for key in self.arms}
+        self.b_a = {key: np.zeros((self.n_context, 1)) for key in self.arms}
+        self.theta_a = {key: np.zeros((self.n_context, 1)) for key in self.arms}
 
+        for arm, rew, us in self.user_log:
+            payoffs = {key: -1 for key in self.Aa}
+
+            mx = -1000000
+            ind = -1
+
+            for key in self.Aa:
+                self.theta_a[key] = np.dot(self.Aa_inv[key], self.b_a[key])
+                payoffs[key] = np.dot(us, self.theta_a[key]) + \
+                               self.alpha * np.sqrt(np.dot(np.dot(us, self.Aa_inv[key]), us.transpose()))
+                ind = ind if mx > payoffs[key] else key
+                mx = mx if mx > payoffs[key] else payoffs[key]
+
+            pred_arm = ind
+
+            if arm == pred_arm:
+                self.Aa[pred_arm] += np.outer(us, us)
+                self.Aa_inv[pred_arm] = np.linalg.inv(self.Aa[pred_arm])
+                self.b_a[pred_arm] += rew * us.transpose()
+                self.theta_a[pred_arm] = self.Aa_inv[pred_arm].dot(self.b_a[pred_arm])
+
+                curr_CTR, self.ctr_num[arm], self.ctr_den[arm] = \
+                    self.calc_CTR(rew, self.ctr_num[arm], self.ctr_den[arm])
+
+                self.CTR[arm][time] = curr_CTR
+                self.CTR_relative[arm][time] = curr_CTR / self.CTR_random[arm]\
+                    if self.CTR_random[arm] > 0 else curr_CTR / 1
+            time += 1
+
+    def calc_CTR(self, reward, ctr_num, ctr_den):
+        ctr_num = ctr_num + reward
+        ctr_den = ctr_den + 1
+
+        ctr = ctr_num / ctr_den
+
+        return ctr, ctr_num, ctr_den
+
+    def calc_random_ctr(self):
+        # CTR of random policy
+        n_shows = {key: 0 for key in self.arms}
+        rews = {key: 0 for key in self.arms}
+
+        for arm, rew, _ in self.user_log:
+            n_shows[arm] += 1
+            rews[arm] += rew
+
+        self.CTR_random = {arm: rews[arm] / n_shows[arm] for arm in self.arms}
+
+    def plot_graphs(self, ls=None):
+        ls = self.arms if ls is None else ls
+        plt.grid()
+        for item in ls:
+            lists = sorted(self.CTR_relative[item].items())
+            x, y = zip(*lists)
+            plt.plot(x, y)
+        plt.show()
